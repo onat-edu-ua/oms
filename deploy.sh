052b57980302cb1ec7f081ea09d2aa02c57a6349
Kirill Shestakov <fakie215@gmail.com>
2018-08-13 16:12:56 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,27 +3,23 @@
 set -e
 
 function usage(){
-	echo -e "usage: $0 api_base release distribution component deb_file1 [ deb_file2 ...]"
+	echo -e "usage: $0 <api_base> <local repo name> <publish prefix>/<publish distribution> deb_file1 [ deb_file2 ...]"
 	exit 1
 }
 
 [ -z "$1" ] && echo -e "\nmissed api base\n" && usage
-[ -z "$2" ] && echo -e "\nmissed release\n" && usage
-[ -z "$3" ] && echo -e "\nmissed distribution\n" && usage
-[ -z "$4" ] && echo -e "\nmissed component\n" && usage
-[ -z "$5" ] && echo -e "\nmissed package\n" && usage
+[ -z "$2" ] && echo -e "\nmissed local repo name\n" && usage
+[ -z "$3" ] && echo -e "\nmissed publish prefix and distribution\n" && usage
+[ -z "$4" ] && echo -e "\nmissed package\n" && usage
 
 api_base="$1"
 shift
-release="$1"
+repo="$1"
 shift
-distr="$1"
-shift
-component="$1"
+pub="$1"
 shift
 
 files="$@"
-repo="${release}_${distr}_${component}"
 timestamp="$(date +%s)"
 dir="${repo}_${timestamp}"
 
@@ -40,6 +36,6 @@ done
 echo -e "\n\e[33m> add pkg to repo $repo\e[39m\n"
 curl -sSf -X POST "${api_base}/repos/$repo/file/$dir" || fail "can't add pkg to repo"
 
-echo -e "\n\e[33m> update publish $release/$distr \e[39m\n"
-curl -sSf -X PUT -H 'Content-Type: application/json' --data '{ "Signing": {"PassphraseFile":"/home/pkg/gpg_pass"} }' "${api_base}/publish/${release}/${distr}" || fail "can't publish"
+echo -e "\n\e[33m> update publish $pub \e[39m\n"
+curl -sSf -X PUT -H 'Content-Type: application/json' --data '{ "Signing": {"PassphraseFile":"/home/pkg/gpg_pass"} }' "${api_base}/publish/${pub}" || fail "can't publish"
 
